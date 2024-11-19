a0d667dd3d5da646ebeaa10498db06f987dcbc6e
Sahil Garg <sahilga@synopsys.com>
2024-11-19 21:06:18 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,13 @@ import minimatch from "minimatch";
 const GITHUB_TOKEN: string = core.getInput("GITHUB_TOKEN");
 const OPENAI_API_KEY: string = core.getInput("OPENAI_API_KEY");
 const OPENAI_API_MODEL: string = core.getInput("OPENAI_API_MODEL");
+const OPENAI_API_ENDPOINT: string = core.getInput("OPENAI_API_ENDPOINT");
 
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
+  baseURL: OPENAI_API_ENDPOINT,
 });
 
 interface PRDetails {
